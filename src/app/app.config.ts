5753e31131885f95aa4dbef272370a09bc0662e5
leandrolbnu <leandrol.bnu@gmail.com>
2026-01-18 17:09:04 -0300

    Commit inicial das configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { UsuarioComponent } from './usuarios/usuarios.component';
+import { HomeComponent } from './home/home.component';
+import { LivrosComponent } from './livros/livros.component';
+import { RecommendacoesComponent } from './recomendacoes/recomendacoes.component';
+import { EmprestimosComponent } from './emprestimos/emprestimos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  { path: 'usuarios', component: UsuarioComponent },
+
+  { path: 'livros', component: LivrosComponent },
+
+  { path: 'emprestimos', component: EmprestimosComponent },
+
+  { path: 'recomendacoes', component: RecommendacoesComponent },
+
+  { path: '**', redirectTo: '' }
+];
